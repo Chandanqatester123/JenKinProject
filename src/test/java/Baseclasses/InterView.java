86d2a7eeb252990ce07fc1b24ca7862b42fc8602
Chandanqatester123 <your-email@example.com>
2026-03-11 21:54:31 +0530

    Code_Changes

--- src/test/java/Baseclasses/InterView.java
+++ src/test/java/Baseclasses/InterView.java
@@ -4,25 +4,4 @@ import java.util.Scanner;
 
 public class InterView {
 	public static void main(String[] args) {
-//		| Code         | Meaning               |
-//		| ------------ | --------------------- |
-//		| `split("")`  | Split into characters |
-//		| `split(" ")` | Split into words      |
-//		| `charAt(i)`  | Access one character  |
-//		| `next()`     | Reads one word        |
-//		| `nextLine()` | Reads full sentence   |
-
-		Scanner sc=new Scanner (System.in);
-		System.out.println("Enter a String");
-		String Sentence=sc.nextLine();
-		
-		String[] Slpit_words=Sentence.split(" ");
-		String reverse_words="";
-		for(int i=Slpit_words.length-1;i>=0; i--)
-		{
-			reverse_words=reverse_words+Slpit_words[i]+" ";
-		}
-		System.out.println(reverse_words);
-	}
-
-}
+}}

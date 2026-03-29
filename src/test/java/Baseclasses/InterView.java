8d9724d63be4bdc0152c5431dfd8846c9a6a44fc
Chandanqatester123 <your-email@example.com>
2026-03-29 17:14:29 +0530

    Chandan

--- src/test/java/Baseclasses/InterView.java
+++ src/test/java/Baseclasses/InterView.java
@@ -2,20 +2,9 @@ package Baseclasses;
 
 import java.util.Scanner;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-
 public class InterView {	
 	public static void main(String[] args)
 	{
-		int a=2;
-		int b=3;
-		int c=4;
-		int d=++a + ++b + ++c + ++a + a+ b+c + ++c;
-		System.out.println(d);
-//		     =3+4+5+4+4+4+5+6;
-	}
-	
-	
-	
+		
+	}	
 }

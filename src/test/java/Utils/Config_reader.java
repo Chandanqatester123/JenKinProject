a5e7378b1a7a954fc54b484d6987157112773f46
Chandanqatester123 <your-email@example.com>
2026-04-13 02:58:50 +0530

    A

--- src/test/java/Utils/Config_reader.java
+++ src/test/java/Utils/Config_reader.java
@@ -8,6 +8,7 @@ import java.util.Properties;
 import org.testng.annotations.Test; // ✅ correct
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
@@ -27,8 +28,8 @@ public class Config_reader {
     @BeforeSuite
     public void Before_Suite() {
         try {
-//            FileInputStream fis = new FileInputStream("src\\test\\resources\\Chandan_New_Test_Data.properties");
-            InputStream fis = getClass().getClassLoader().getResourceAsStream("src\\\\test\\\\resources\\\\Chandan_New_Test_Data.properties");
+            FileInputStream fis = new FileInputStream("src\\test\\resources\\Chandan_New_Test_Data.properties");
+//            InputStream fis = getClass().getClassLoader().getResourceAsStream("src\\\\test\\\\resources\\\\Chandan_New_Test_Data.properties");
             p = new Properties();
             p.load(fis);
         } catch (IOException e) {
@@ -38,7 +39,13 @@ public class Config_reader {
     
     @BeforeMethod
     public void Before_Method() {
-        driver=new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("--headless=new");
+//        options.addArguments("--no-sandbox");
+//        options.addArguments("--disable-dev-shm-usage");
+        driver = new ChromeDriver(options);
+        
+//        driver=new ChromeDriver();
         URL=p.getProperty("URL");
         driver.get((URL));
         driver.manage().window().maximize();

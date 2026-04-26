5b7704cf112765061054b6e5b7dd4967a8016726
Chandanqatester123 <your-email@example.com>
2026-04-27 00:02:00 +0530

    Shreekant

--- src/test/java/Baseclasses/Shreekant.java
+++ src/test/java/Baseclasses/Shreekant.java
@@ -1,8 +1,32 @@
 package Baseclasses;
 
+import java.io.IOException;
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
 public class Shreekant { 
-	public static void main(String[] args) {
+	@Test 
+	public void M1() throws InterruptedException, IOException {
 		System.out.println("Chandan");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://the-internet.herokuapp.com/upload");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(10));
+		WebElement Upload=wait.until(ExpectedConditions.elementToBeClickable((By.id("drag-drop-upload"))));
+		Upload.click();
+		Thread.sleep(1000);
+		Runtime.getRuntime().exec("C:\\Users\\chand\\Music\\FileUploadPopup.exe");
+		
+		Thread.sleep(5000);
+		
+		
 	}
 
 }

d8010191a04824d479f81bc3606d9b54ea55a9bd
Chandanqatester123 <your-email@example.com>
2026-04-27 00:10:20 +0530

    Chandan

---
+++ src/test/java/Java_Programms_Practice/Upload_popup.java
@@ -0,0 +1,34 @@
+package Java_Programms_Practice;
+
+import java.io.IOException;
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+public class Upload_popup {
+	
+	@Test 
+	public void M1() throws InterruptedException, IOException {
+		System.out.println("Chandan");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://the-internet.herokuapp.com/upload");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(10));
+		WebElement Upload=wait.until(ExpectedConditions.elementToBeClickable((By.id("drag-drop-upload"))));
+		Upload.click();
+		Thread.sleep(1000);
+		Runtime.getRuntime().exec("C:\\Users\\chand\\Music\\FileUploadPopup.exe");
+		
+		Thread.sleep(5000);
+		
+		
+	}
+
+
+}
